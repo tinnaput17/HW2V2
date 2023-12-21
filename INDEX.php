2e10c85c1e269f0f30a742c6291972f26300562f
TNP <65160330@go.buu.ac.th>
2023-12-21 17:22:01 +0700

    Create INDEX.php

---
+++ INDEX.php
@@ -0,0 +1,35 @@
+<html>
+    <body>
+        <h1>This is PHP <?php 
+        echo 'Hello world!'; ?>
+        <div>
+            <p>1</p>
+            <?php
+            $var_a =1;
+            $var_a = "1 $var_a";
+            print_r($var_a);
+            echo "<br>";
+            #var_dump($var_a);
+            echo $var_a . "2";
+            echo "<br>";
+            $array =array();
+            $array =[];
+            $array[]=1;
+            $array[]=2;
+            $array[0]=3;
+            print_r($array);
+            //count
+            //size_of
+            for($i=0;$i<count($array);$i++)
+            {
+                echo"<h1>my h1 </h1>";
+                ?>
+                <p>2</p>
+                <?php
+            }
+            ?>
+            <p>3</p>
+        </div>
+</h>
+</body>
+</html>
