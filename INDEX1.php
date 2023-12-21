cab10197e34c3834202c1061cee00ec72124e23d
TNP <65160330@go.buu.ac.th>
2023-12-21 17:49:40 +0700

    Test1

--- INDEX1.php
+++ INDEX1.php
@@ -2,8 +2,21 @@
     <body>
         <h1>This is PHP <?php 
         echo 'Hello world!<br><br>'; ?>
-        <?php $var_a =""?>
-        
+        <?php $var_a ="SE CAMP"?>
+        <?php echo $var_b ='Hello $var_a'?>
+        <br>
+        <?php echo $var_c = "Hello $var_a"?>
+        <br>
+        <?php 
+        $varc += $var_b = 1;
+        if($var_b<==$var_c){
+            echo '$var_b<==$var_c';
+        }else if ($var_b<>$var_c){
+            echo '$var_b<==$var_c';
+        }else{
+
+        }?>
+
 
        
 </h>
