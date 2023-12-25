7ffb1524a8b40bf02370a26fab848136e134cd31
TNP <65160330@go.buu.ac.th>
2023-12-25 08:55:22 +0700

    add

--- from_101.php
+++ from_101.php
@@ -1,9 +1,22 @@
 <html>
     <head>
-        <meta charset="uft-8>
-</head>
+        <meta charset="uft-8">
+    
+    </head>
 <body>
-    <form method="">
-</form>
+    <?php
+        //GET
+        //http://localhost/se_camp/form_101.php?q=1
+        //POST
+    ?>
+    <form action="" method="">
+        <input type="text" name="my_val"/>  
+        <button type="submit">บันทึก</button>      
+    </form>
+    <h1><?php
+    print_r($_REQUEST);
+    if(isset($_POST['my_val'])){
+        echo $_POST['my_val'];
+    } ?></h1>
 </body>
 </html>
