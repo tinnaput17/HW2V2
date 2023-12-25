79bf02a2f5047e33407a3fb153d13a283f8ecd9f
TNP <65160330@go.buu.ac.th>
2023-12-25 19:33:39 +0700

    Add

---
+++ example-app/resources/views/myroute.blade.php
@@ -0,0 +1,9 @@
+<html>
+    <head>
+        <meta charset="utf-8"/>
+    </head>
+    <body>
+        <h1> My Route Page </h1>
+        <h1>{{$myinput}}</h1>
+    </body>
+</html>
--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Http\Client\Request as ClientRequest;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +17,17 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+route::get('/my-route',function(){
+    //return view('myroute');
+    //          Key =>  Value
+    $data = ['val_a'=>'Hello World!'];
+    $data['val_b'] = "Lalaval";
+    return view('myfolder.mypage',$data);
+});
+route::post('my-route',function(Request $req){
+    //echo $req ->input('myinput');
+    $data['myinput']=$req->input('myinput');
+    return view('myroute',$data);
 });
